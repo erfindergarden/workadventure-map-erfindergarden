f6d0ac5bd6070b3d0c912516802c00a68eee3851
Maximilian Laurenz <max@slmedien.de>
2021-03-13 18:51:25 +0100

    Added tiles and map test.

---
+++ officeZeugs.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="officeZeugs" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="officeZeugs.png" width="512" height="512"/>
+ <tile id="48">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.44707" y="0.66233" width="21.0053" height="31.0349"/>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.42105" y="12.3004" width="21.1946" height="11.0704"/>
+  </objectgroup>
+ </tile>
+</tileset>
